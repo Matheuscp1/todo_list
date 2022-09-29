a3cdc4b205be53b36a013b369bc8263b93c98c88
Matheus Campos <matheusproprentner@gmail.com>
2022-09-29 19:07:48 -0300

    feat: add todo list

---
+++ lib/model/Todo.dart
@@ -0,0 +1,7 @@
+// ignore_for_file: empty_constructor_bodies
+
+class Todo {
+  Todo({required this.title}) {}
+  String title;
+  DateTime dateTime = DateTime.now();
+}
--- lib/pages/todo_list_page.dart
+++ lib/pages/todo_list_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:todo_list/model/Todo.dart';
+import 'package:todo_list/widgets/todo_list_item.dart';
 
 class TodoListPage extends StatefulWidget {
   TodoListPage({Key? key}) : super(key: key);
@@ -10,81 +12,87 @@ class TodoListPage extends StatefulWidget {
 class _TodoListPageState extends State<TodoListPage> {
   final TextEditingController _addController = TextEditingController();
 
-  List<String> todo = [];
+  List<Todo> todo = [];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-          child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: Column(mainAxisSize: MainAxisSize.min, children: [
-          Row(children: [
-            Expanded(
-              child: TextField(
-                controller: _addController,
-                keyboardType: TextInputType.text,
-                decoration: InputDecoration(
-                    hoverColor: Color(0xff00d7f3),
-                    prefixIcon: Icon(Icons.add_box),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide:
-                          BorderSide(color: Color(0xff00d7f3), width: 2.0),
-                    ),
-                    border: OutlineInputBorder(
-                        borderSide:
-                            BorderSide(width: 10, color: Color(0xff00d7f3))),
-                    labelText: "Adicione uma tarefa",
-                    hintText: "Todo"),
-              ),
-            ),
-            SizedBox(
-              width: 8,
-            ),
-            ElevatedButton(
-              onPressed: () {
-                String text = _addController.text;
-                print(todo);
-                setState(() {
-                  todo.add(text);
-                });
-              },
-              style: ElevatedButton.styleFrom(
-                  backgroundColor: Color(0xff00d7f3),
-                  padding: EdgeInsets.all(14)),
-              child: Icon(Icons.add, size: 30),
-            )
-          ]),
-          SizedBox(
-            height: 16,
-          ),
-          SizedBox(
-            height: 200,
-            child: ListView.builder(
-                shrinkWrap: true,
-                itemCount: todo.length,
-                itemBuilder: (context, index) => ListTile(
-                      title: Text(todo[index]),
-                      style: ListTileStyle.list,
-                      onTap: (() => print(todo[index])),
-                    )),
-          ),
-          Row(
-            children: [
-              Expanded(child: Text('Você possui 0 tarefas pendentes')),
-              SizedBox(
-                width: 8,
-              ),
-              ElevatedButton(
-                  onPressed: () {},
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Center(
+              child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+            child: Column(mainAxisSize: MainAxisSize.min, children: [
+              Row(children: [
+                Expanded(
+                  child: TextField(
+                    controller: _addController,
+                    keyboardType: TextInputType.text,
+                    decoration: InputDecoration(
+                        hoverColor: Color(0xff00d7f3),
+                        prefixIcon: Icon(Icons.add_box),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Color(0xff00d7f3), width: 2.0),
+                        ),
+                        border: OutlineInputBorder(
+                            borderSide: BorderSide(
+                                width: 10, color: Color(0xff00d7f3))),
+                        labelText: "Adicione uma tarefa",
+                        hintText: "Todo"),
+                  ),
+                ),
+                SizedBox(
+                  width: 8,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    String text = _addController.text;
+                    Todo todoItem = Todo(title: text);
+                    print(todo);
+                    setState(() {
+                      todo.add(todoItem);
+                    });
+                  },
                   style: ElevatedButton.styleFrom(
                       backgroundColor: Color(0xff00d7f3),
                       padding: EdgeInsets.all(14)),
-                  child: Text('Limpar tudo'))
-            ],
-          )
-        ]),
-      )),
+                  child: Icon(Icons.add, size: 30),
+                ),
+              ]),
+              SizedBox(
+                height: 5,
+              ),
+              SizedBox(
+                height: 640,
+                child: ListView.builder(
+                    itemCount: todo.length,
+                    itemBuilder: (context, index) =>
+                        TodoListItem(todo: todo[(todo.length - 1) - index])),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Row(
+                children: [
+                  Expanded(
+                      child:
+                          Text('Você possui ${todo.length} tarefas pendentes')),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Color(0xff00d7f3),
+                          padding: EdgeInsets.all(14)),
+                      child: Text('Limpar tudo'))
+                ],
+              )
+            ]),
+          )),
+        ),
+      ),
     );
   }
 }
